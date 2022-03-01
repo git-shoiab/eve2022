e1fe217da271613ed55222e72010320b080c6d8e
hit300b1 <hit300b1@gmail.com>
2022-03-01 07:45:14 +0530

    1-mar

---
+++ eve/src/ch6/StaticDemo2.java
@@ -0,0 +1,35 @@
+package ch6;
+
+public class StaticDemo2 {
+	public static void main(String[] args) {
+		//StatBlock obj=new StatBlock();
+		//obj.met();
+		//obj.met2();
+		
+		StatBlock.metS();
+		StatBlock.metSS();
+	}
+}
+
+
+class StatBlock{
+	public StatBlock() {
+		System.out.println("cons called...");
+	}
+	static {
+		System.out.println("static block called....");
+	}
+	void met() {
+		System.out.println("met called....");
+	}
+	void met2() {
+		System.out.println("met22222222222222 called....");
+	}
+	
+	static void metS() {
+		System.out.println("static met called...");
+	}
+	static void metSS() {
+		System.out.println("staticsssssssss met called...");
+	}
+}
